09a9948150364c26c394b60a6fc06cf6ba2789e8
jackaranaram <104057685+jackaranaram@users.noreply.github.com>
2025-06-18 23:24:04 -0500

    chore: add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,23 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from './task/task.module';
 import { Task } from './task/task.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'todolist',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Task],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     TaskModule
   ],
